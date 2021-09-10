46da638906c31daa59af819607349b33177814f3
gouri bhise <gouri.a.bhise@gmail.com>
2021-09-10 15:18:22 +0530

    removed href attribute from anchor tag

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,13 +7,13 @@ export default function Navbar(){
    <img src={logo} alt="city tours logo"/>
    <ul className="nav-links">
       <li>
-        <a href="#" className="nav-link">Home</a>
+        <a  className="nav-link">Home</a>
       </li>
       <li>
-        <a href="#" className="nav-link">About</a>
+        <a className="nav-link">About</a>
       </li>
       <li>
-        <a href="#" className="nav-link active">Tours</a>
+        <a className="nav-link active">Tours</a>
       </li>
 
    </ul>
