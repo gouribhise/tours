1d171e0b84893a5ab078c60c9a996fc1a0770f6a
gouri bhise <gouri.a.bhise@gmail.com>
2021-09-10 13:15:46 +0530

    added scss style to tour section

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -1,8 +1,30 @@
 import React,{Component} from 'react'
+import './Tour.scss';
  export default class Tour extends Component{
   render(){
     return(
-      <div>Single tour</div>
+      <article className="tour">
+      <div className="img-container">
+         <img
+       
+           src="https://images.pexels.com/photos/3586966/pexels-photo-3586966.jpeg"
+           alt=""/>
+           <span className="close-btn">
+           <i className="fas fa-window-close"/>
+           </span>
+      </div>
+      <div className="tour-info">
+      <h3>city</h3>
+      <h4>name</h4>
+      <h5>info<span>
+      <i className="fas fa-caret-square-down"/>
+      </span>
+      </h5>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, debits.
+      </p>
+
+      </div>
+      </article>
     )
   }
 }
--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -4,8 +4,8 @@ import Tour from '../Tour/Tour'
 export default class TourList extends Component{
   render(){
     return(
-      <section>
-   
+      <section className="tourlist">
+
          <Tour/>
       </section>
     )
