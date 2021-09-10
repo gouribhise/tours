61d9c5148ccf24a7e5676796d33ed30474eb440a
gouri bhise <gouri.a.bhise@gmail.com>
2021-09-10 14:39:36 +0530

    hide and show info

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -3,6 +3,11 @@ import './Tour.scss';
  export default class Tour extends Component{
    state={
      showInfo:false
+   };
+   handleInfo=()=>{
+     this.setState({
+       showInfo:!this.state.showInfo
+     })
    }
   render(){
     const{city,img,name,info}=this.props.tour;
@@ -20,12 +25,12 @@ import './Tour.scss';
       <div className="tour-info">
       <h3>{city}</h3>
       <h4>{name}</h4>
-      <h5>info<span>
+      <h5>info<span onClick={this.handleInfo}>
       <i className="fas fa-caret-square-down"/>
       </span>
       </h5>
-      {this.state.showInfo}
-      <p>{info}      </p>
+      {this.state.showInfo &&     <p>{info}      </p>}
+
 
       </div>
       </article>
