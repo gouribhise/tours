b1073ab48739644566743ee50701c84688fc087d
gouri bhise <gouri.a.bhise@gmail.com>
2021-09-10 14:15:05 +0530

    displaying list of data

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -2,26 +2,26 @@ import React,{Component} from 'react'
 import './Tour.scss';
  export default class Tour extends Component{
   render(){
+    const{city,img,name,info}=this.props.tour;
     return(
       <article className="tour">
       <div className="img-container">
          <img
-       
-           src="https://images.pexels.com/photos/3586966/pexels-photo-3586966.jpeg"
+
+           src={img}
            alt=""/>
            <span className="close-btn">
            <i className="fas fa-window-close"/>
            </span>
       </div>
       <div className="tour-info">
-      <h3>city</h3>
-      <h4>name</h4>
+      <h3>{city}</h3>
+      <h4>{name}</h4>
       <h5>info<span>
       <i className="fas fa-caret-square-down"/>
       </span>
       </h5>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, debits.
-      </p>
+      <p>{info}      </p>
 
       </div>
       </article>
